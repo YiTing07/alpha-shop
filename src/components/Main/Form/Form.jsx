c218b6293bc97d081141f9dcd9bf024feeafc396
unknown <hevecandy@gmail.com>
2023-11-03 21:56:03 +0800

    build: add Step2.jsx & Step2.module.scss

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,7 +1,7 @@
 import styles from './Form.module.scss';
 //import StepProgress from './StepProgress';
-import Step1 from './Step1';
-//import Step2 from './Step2';
+//import Step1 from './Step1';
+import Step2 from './Step2';
 //import Step3 from './Step3';
 //import ProgressControl from './ProgressControl';
 
@@ -13,7 +13,7 @@ export default function Form () {
       <div className={styles.mainContainer}>
         <section className={`${styles.registerContainer} col col-lg-6 col-sm-12`} data-phase="1" data-total-price="0">
           <h2 className={`${styles.registerTitle} col col-12`}>結帳</h2>
-          <Step1></Step1>
+          <Step2></Step2>
         </section>
       </div>
     </main>
