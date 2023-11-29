a170cd3e5a9f11b57b97459a2254effa1d862215
YiTing07 <hevecandy@gmail.com>
2023-11-29 22:46:35 +0800

    feat: add CartItems props

--- src/components/Main/Cart/Cart.jsx
+++ src/components/Main/Cart/Cart.jsx
@@ -2,23 +2,27 @@ import styles from './Cart.module.scss';
 import { ReactComponent as Minus } from '../../../assets/icons/minus.svg';
 import { ReactComponent as Plus } from '../../../assets/icons/plus.svg';
 
-function Product() {
+function CartItems ({ cartInfo }) {
   return (
     <>
-      {products.map(item =>
-      <div className={`${styles.productContainer} col col-12`}>
+      {cartInfo.map(item =>
+      <div className={`${styles.productContainer} col col-12`} key={item.id}>
         <img
-        key={item.id} 
-        className={styles.imgContainer}
-        src={item.img} 
-        alt={item.name}/>
+          className={styles.imgContainer}
+          src={item.img} 
+          alt={item.name}
+        />
         <div className={styles.productInfo}>
           <div className={styles.productName}>{item.name}</div>
           <div className={styles.productControlContainer}>
             <div className={styles.productControl}>
-              <Minus className={styles.productAction}></Minus>
+              <Minus
+                className={styles.productAction}
+              />
               <span className={styles.productCount}>{item.quantity}</span>
-              <Plus className={styles.productAction}></Plus>
+              <Plus
+                className={styles.productAction}
+              />
             </div>
           </div>
           <div className={styles.price}>{item.price}</div>
@@ -30,13 +34,15 @@ function Product() {
   );
 }
 
-export default function Cart() {
+export default function Cart({ cartInfo }) {
   return (
     <section className={`${styles.cartContainer} col col-lg-5 col-sm-12`}>
       <h3 className={styles.cartTitle}>購物籃</h3>
 
       <section className={`${styles.productList} col col-12`} data-total-price="0">
-        <Product></Product>
+        <CartItems
+          cartInfo={cartInfo}
+        />
       </section>
 
       <section className={`${styles.cartInfo} shipping col col-12`}>
