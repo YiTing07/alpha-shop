d7fa0c981eaf6c170dafaef977e6a2525126ea10
YiTing07 <hevecandy@gmail.com>
2023-11-29 22:33:49 +0800

    feat: add state to store cartInfo

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -2,9 +2,12 @@ import styles from './Main.module.scss';
 
 import Form from './Form/Form';
 import Cart from './Cart/Cart';
+import { useState } from 'react';
 
 
 export default function Main () {
+  const [cartinfo, setCartInfo] = useState('');
+
   return (
     <main className={styles.siteMain}>
       <div className={styles.mainContainer}>
