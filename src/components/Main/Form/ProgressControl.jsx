0dff9fed1a2591145c6a75390bffb00619a46d3a
YiTing07 <hevecandy@gmail.com>
2023-11-29 21:53:30 +0800

    feat: add ControlBtn prop

--- src/components/Main/Form/ProgressControl.jsx
+++ src/components/Main/Form/ProgressControl.jsx
@@ -2,18 +2,51 @@ import styles from './ProgressControl.module.scss'
 import { ReactComponent as LeftArrow } from '../../../assets/icons/left-arrow.svg';
 import { ReactComponent as RightArrow } from '../../../assets/icons/right-arrow.svg';
 
-export default function ProgressControl () {
+function ControlBtn ({step, handleClickNext, handleClickPrev}) {
+  if (step === 1) {
+    return (
+      <button className={styles.next} onClick={handleClickNext}>
+        下一步
+        <RightArrow />
+      </button>
+    )
+  } else if (step === 2) {
+    return (
+      <>
+        <button className={styles.prev} onClick={handleClickPrev}>
+          <LeftArrow />
+            上一步
+        </button>
+        <button className={styles.next} onClick={handleClickNext}>
+            下一步
+          <RightArrow />
+        </button>
+      </>
+    )
+  } else if (step === 3) {
+    return (
+      <>
+        <button className={styles.prev} onClick={handleClickPrev}>
+          <LeftArrow />
+            上一步
+        </button>
+        <button className={styles.next} onClick={handleClickNext}>
+          確認下單
+        </button>
+      </>
+    )
+  }
+}
+
+export default function ProgressControl ({step, handleClickNext, handleClickPrev}) {
     return (
       <section className={`${styles.progressControlContainer} col col-lg-6 col-sm-12`}>
         <section className={`${styles.buttonGroup} col col-12`}>
-          <button className={styles.prev}>
-            <LeftArrow />
-              上一步
-          </button>
-          <button className={styles.next}>
-              下一步
-            <RightArrow />
-          </button>
+          <ControlBtn
+            step={step}
+            handleClickNext={handleClickNext}
+            handleClickPrev={handleClickPrev}
+          />
         </section>
       </section>
     );
