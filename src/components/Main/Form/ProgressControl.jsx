b72d8c5de0041f5b336beb4b232beb38be03658b
YiTing07 <hevecandy@gmail.com>
2024-07-17 15:20:16 +0800

    feat: modify ControlBtn function

--- src/components/Main/Form/ProgressControl.jsx
+++ src/components/Main/Form/ProgressControl.jsx
@@ -4,60 +4,45 @@ import { ReactComponent as RightArrow } from '../../../assets/icons/right-arrow.
 import { useContext } from 'react';
 import { FormContext } from '../../../contexts/FormContext';
 
-function ControlBtn ({step, handleStepClick, handleClickOrder}) {
-  if (step === 1) {
-    return (
-      <button className={styles.next} onClick={() => handleStepClick(1)}>
-            下一步
-        <RightArrow />
-      </button>
-    )
-  } else if (step === 2) {
-    return (
-      <>
+function ControlBtn({ step, handleStepClick, handleClickOrder }) {
+  return (
+    <>
+      {step > 1 && (
         <button className={styles.prev} onClick={() => handleStepClick(-1)}>
           <LeftArrow />
-            上一步
+          上一步
         </button>
+      )}
+      {step < 3 ? (
         <button className={styles.next} onClick={() => handleStepClick(1)}>
-            下一步
+          下一步
           <RightArrow />
         </button>
-      </> 
-    )
-  } else if (step === 3) {
-    return (
-      <>
-        <button className={styles.prev} onClick={() => handleStepClick(-1)}>
-          <LeftArrow />
-            上一步
-        </button>
+      ) : (
         <button className={styles.next} onClick={handleClickOrder}>
           確認下單
         </button>
-      </>
-    )
-  }
+      )}
+    </>
+  )
 }
 
-export default function ProgressControl ({step, handleStepClick}) {
-  const {logInputValues} = useContext(FormContext)
+export default function ProgressControl({ step, handleStepClick }) {
+  const { logInputValues } = useContext(FormContext)
 
   const handleClickOrder = () => {
     logInputValues();
   }
-  
-    return (
-      <section className={`${styles.progressControlContainer} col col-lg-6 col-sm-12`}>
-        <section className={`${styles.buttonGroup} col col-12`}>
-          <ControlBtn 
-            step={step} 
-            // handleClickPrev={handleClickPrev} 
-            // handleClickNext={handleClickNext}
-            handleStepClick={handleStepClick}
-            handleClickOrder={handleClickOrder}
-          />
-        </section>
+
+  return (
+    <section className={`${styles.progressControlContainer} col col-lg-6 col-sm-12`}>
+      <section className={`${styles.buttonGroup} col col-12`}>
+        <ControlBtn
+          step={step}
+          handleStepClick={handleStepClick}
+          handleClickOrder={handleClickOrder}
+        />
       </section>
-    );
-  }
+    </section>
+  );
+}
